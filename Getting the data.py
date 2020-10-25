bee74392f1cd17ed1f5878f609bfc95fab11df66
Arockiya Selva <arockiyaselva@gmail.com>
2020-10-25 08:15:13 +0400

    Started Practicing

--- Getting the data.py
+++ Getting the data.py
@@ -1,1 +1,11 @@
-print(123)
+import pandas as pd
+print("Pandas imported")
+
+data=pd.read_csv('Resources/pokemon_data.csv')
+print(data)
+
+# Top 5
+print(data.head(5))
+
+# Bottom 5
+print(data.tail(5))
