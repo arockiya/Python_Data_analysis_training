1b20e1daf194872c224244ff5c0f1b2b41cb221a
Arockiya Selva <arockiyaselva@gmail.com>
2020-10-25 08:24:50 +0400

    Reading from CSV

--- Getting the data.py
+++ Getting the data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 print("Pandas imported")
 
-data=pd.read_csv('Resources/pokemon_data.csv')
-print(data)
+#data=pd.read_csv('Resources/pokemon_data.csv')
+#d_xlsx=pd.read_excel("Resources/pokemon_data.xlsx")
+#print(data)
+#print(d_xlsx)
 
 # Top 5
-print(data.head(5))
+#print(d_xlsx.head(5))
 
 # Bottom 5
-print(data.tail(5))
+#print(d_xlsx.tail(5))
--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 Help Tutorial: https://www.youtube.com/watch?v=r-uOLxNrNk8
 
+Help Tutorial 2: https://www.youtube.com/watch?v=vmEHCJofslg (Currently following)
+
 Parts:
 1. Getting the data
+2. Reading the data from CSV file
+3. Reading the data from Excel file
+4. Reading the data from Text file
